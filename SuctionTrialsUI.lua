cd703e1429cccd1d0c4b2bedb9ce913e298d4644
water.5202 <yeahidkorb@gmail.com>
2025-07-15 17:15:27 +0100

    SuctionTrialsUI.lua

--- SuctionTrialsUI.lua
+++ SuctionTrialsUI.lua
@@ -1,7 +1,7 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
+    Title = "Suction Trials // " .. identifyexecutor(),
+    SubTitle = "",
     TabWidth = 160,
     Size = UDim2.fromOffset(400, 300),
     Acrylic = false,
@@ -13,7 +13,7 @@ local Tabs = {
     Main = Window:AddTab({ Title = "Plunger Badge", Icon = "hammer" })
 }
 
-Tab:AddButton({
+Tabs.Main:AddButton({
     Title = "Get Plunger Badge",
     Description = "placeholder :3",
     Callback = function()
