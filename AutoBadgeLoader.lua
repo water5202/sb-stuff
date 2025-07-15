285c5fb0189ebd846b081e621e9364849f279825
water.5202 <yeahidkorb@gmail.com>
2025-07-15 13:07:08 +0100

    Main

---
+++ AutoBadgeLoader.lua
@@ -0,0 +1,199 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Players = game:GetService("Players")
+local localplayer = Players.LocalPlayer
+local UIS = game:GetService("UserInputService")
+
+if game.Workspace:FindFirstChild("FishAutoFarmPart") then 
+    game.Workspace.FishAutoFarmPart:Destroy() 
+end
+
+local Platform = Instance.new("Part")
+Platform.Name = "FishAutoFarmPart"
+Platform.Parent = workspace
+Platform.CFrame = CFrame.new(17893.6895, -122.158432, -2288.86475, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+Platform.Size = Vector3.new(84, 1, 82)
+Platform.Transparency = 0.5
+Platform.Anchored = true
+
+Fluent:Notify({
+        Title = "Interface",
+        Content = "Platform Loaded!",
+        SubContent = "",
+        Duration = 3
+})
+
+local Window = Fluent:CreateWindow({
+    Title = "Auto Fish // " .. identifyexecutor(),
+    SubTitle = "",
+    TabWidth = 120,
+    Size = UDim2.fromOffset(400, 300),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.H
+})
+
+local Tabs = {
+    AutoFish = Window:AddTab({ Title = "AutoFish", Icon = "hammer" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local PlayerText = Tabs.AutoFish:AddParagraph({
+        Title = "0/16",
+        Content = "Players"
+})
+
+task.spawn(function()
+    while true do
+    local playersinnumb = #Players:GetChildren()
+        PlayerText:SetTitle(playersinnumb .. "/16")
+        task.wait(0.001)
+    end
+end)
+
+local TimeBar = Tabs.Settings:AddParagraph({
+    Title = "",
+    Content = "Time taken"
+})
+
+local startTime = tick()
+
+task.spawn(function()
+    while true do
+        local elapsedTime = math.floor(tick() - startTime)
+
+        local hours = math.floor(elapsedTime / 3600)
+        local minutes = math.floor((elapsedTime % 3600) / 60)
+        local seconds = elapsedTime % 60
+
+        local timeString = string.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+        TimeBar:SetTitle(timeString)
+
+        task.wait(1)
+    end
+end)
+
+
+
+Tabs.AutoFish:AddButton({
+        Title = "Equip ZZZ Glove",
+        Description = "",
+        Callback = function()
+            Window:Dialog({
+                Title = "Confirm Equip?",
+                Content = "",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            fireclickdetector(workspace.Lobby.ZZZZZZZ.ClickDetector)
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                        end
+                    }
+                }
+            })
+        end
+})
+
+Tabs.AutoFish:AddButton({
+        Title = "Goto SafeSpot",
+        Description = "",
+        Callback = function()
+            Window:Dialog({
+                Title = "Confirm Teleport?",
+                Content = "",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            localplayer.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(17893.6895, -115.158432, -2288.86475, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                        end
+                    }
+                }
+            })
+        end
+})
+
+Tabs.Settings:AddButton({
+        Title = "Reset",
+        Description = "",
+        Callback = function()
+            Window:Dialog({
+                Title = "Confirm Reset?",
+                Content = "",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            game.Players.LocalPlayer.Character.Humanoid.Health = 0
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                        end
+                    }
+                }
+            })
+        end
+})
+
+Tabs.Settings:AddButton({
+        Title = "Kick",
+        Description = "",
+        Callback = function()
+            Window:Dialog({
+                Title = "Confirm Kick?",
+                Content = "",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            localplayer:Kick("Kicked // " .. localplayer)
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                        end
+                    }
+                }
+            })
+        end
+})
+
+Tabs.Settings:AddButton({
+        Title = "Unload UI",
+        Description = "",
+        Callback = function()
+            Window:Dialog({
+                Title = "Confirm?",
+                Content = "",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            game.Workspace:WaitForChild("FishAutoFarmPart"):Destroy()
+                            Fluent:Destroy()
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                        end
+                    }
+                }
+            })
+        end
+})
+
+Window:SelectTab(1)
