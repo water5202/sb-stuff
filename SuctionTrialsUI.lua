644f3418917faa3e45c34add09229ca83f0df2d0
water.5202 <yeahidkorb@gmail.com>
2025-07-15 19:29:56 +0100

    SuctionTrialsUI.lua

--- SuctionTrialsUI.lua
+++ SuctionTrialsUI.lua
@@ -1,26 +1,15 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local Window = Fluent:CreateWindow({
-    Title = "Suction Trials // " .. identifyexecutor(),
-    SubTitle = "",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(400, 300),
-    Acrylic = false,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.H
+Fluent:Notify({
+        Title = "Interface",
+        Content = "Getting Glove...",
+        SubContent = "",
+        Duration = 3
 })
-
-local Tabs = {
-    Main = Window:AddTab({ Title = "Plunger Badge", Icon = "hammer" })
-}
-
-Tabs.Main:AddButton({
-    Title = "Get Plunger Badge",
-    Description = "placeholder :3",
-    Callback = function()
-        if game.Workspace:FindFirstChild("plunger glove") then
-        fireclickdetector(workspace["plunger glove"].ClickDetector)
-            end
-    end
+fireclickdetector(workspace["plunger glove"].ClickDetector)
+task.wait(1)
+Fluent:Notify({
+        Title = "Interface",
+        Content = "Success!",
+        SubContent = "",
+        Duration = 3
 })
-
-Window:SelectTab(1)
