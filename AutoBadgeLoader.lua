debce356c3505d5e22008d72753f2dc65ec10e4c
water.5202 <yeahidkorb@gmail.com>
2025-07-16 16:18:38 +0100

    AutoBadgeLoader.lua

--- AutoBadgeLoader.lua
+++ AutoBadgeLoader.lua
@@ -203,7 +203,7 @@ firetouchinterest(HRP,  ArenaPart, 0)
 task.wait(0.1)
 firetouchinterest(HRP, ArenaPart, 1)
 task.wait(0.5)
-localplayer.Character.HumanoidRootPart.CFrame = CFrame.new(-108004.688, -116.882454, -2901.13306, 1, 0.000118658187, -2.09227746e-05, -1.09000396e-12, 0.173649266, 0.984807551, 0.000120488701, -0.984807551, 0.173649266)
+localplayer.Character.HumanoidRootPart.CFrame = CFrame.new(-108000.688, -116.882454, -2901.13306, 1, 0.000118658187, -2.09227746e-05, -1.09000396e-12, 0.173649266, 0.984807551, 0.000120488701, -0.984807551, 0.173649266)
 task.wait(0.01)
 localplayer.Character.HumanoidRootPart.Anchored = true
     end
