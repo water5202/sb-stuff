a2ef7f22920d7b196c566d25528dc893a0d1161a
water.5202 <yeahidkorb@gmail.com>
2025-07-28 16:12:01 +0100

    NullZoneUI.lua

--- NullZoneUI.lua
+++ NullZoneUI.lua
@@ -1,3 +1,41 @@
 if not game:IsLoaded() then
 game.Loaded:Wait()
 end
+
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "The Null Zone // " .. identifyexecutor(),
+    SubTitle = "",
+    TabWidth = 120,
+    Size = UDim2.fromOffset(400, 300),
+    Acrylic = false,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.H
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Null Zone Badges", Icon = "hammer" })
+}
+
+Tabs.Main:AddButton({
+    Title = "Get Null Glove",
+    Description = "",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Model.Position
+      task.wait(0.5)
+      fireclickdetector(workspace.Model.Handle.ClickDetector)
+    end
+})
+
+Tabs.Main:AddButton({
+    Title = "Get Tinkerer Glove",
+    Description = "",
+    Callback = function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace._ugcQuestObjectBobPlushie.Position -- Models cant have CFrames
+task.wait(0.5)
+fireclickdetector(workspace._ugcQuestObjectBobPlushie.ClickDetector)
+    end
+})
+
+Window:SelectTab(1)
