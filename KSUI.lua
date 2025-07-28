f179411ad19c5dff5e5c1756695d242163754cae
water.5202 <yeahidkorb@gmail.com>
2025-07-28 17:00:06 +0100

    KSUI.lua

--- KSUI.lua
+++ KSUI.lua
@@ -8,8 +8,6 @@ local bypass;
     bypass = hookmetamethod(game, "__namecall", function(method, ...) 
         if getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Ban then
             return
-        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.AdminGUI then
-            return
         elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.WalkSpeedChanged then
             return
         end
