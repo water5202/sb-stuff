73c8f1e055c92cbd67ee906644777b1711f263e9
water.5202 <yeahidkorb@gmail.com>
2025-07-31 13:18:25 +0100

    GuideUI.lua

--- GuideUI.lua
+++ GuideUI.lua
@@ -38,7 +38,7 @@ Tabs.Main:AddButton({
     Title = "Get Heart + Healing",
     Description = "",
     Callback = function()
-if game.Players.LocalPlayer.Character.CFrame ~= CFrame(3259.35181, -74.5, 821.916199, 0.00376401632, -5.12010487e-08, 0.999992907, 2.30775996e-10, 1, 5.12005442e-08, -0.999992907, 3.80546705e-11, 0.00376401632) then
+if game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame ~= CFrame(3259.35181, -74.5, 821.916199, 0.00376401632, -5.12010487e-08, 0.999992907, 2.30775996e-10, 1, 5.12005442e-08, -0.999992907, 3.80546705e-11, 0.00376401632) then
   return Fluent:Notify({Title = "Interface",Content = "Incorrect CFrame",SubContent = "",Duration = 3})
     else
 firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace:FindFirstChild("Big Heart"), 0)
@@ -61,9 +61,9 @@ local slappinger = Tabs.Main:AddToggle("TheSlappingToggle", {
         if slapping then
             task.spawn(function()
                 while slapping do
-game:GetService("VirtualInputManager"):SendMouseButtonEvent(1000, 1000, Enum.UserInputType.MouseButton1, true, 0)
+game:GetService("VirtualInputManager"):SendMouseButtonEvent(1000, 1000, 1, true, game, 1)
 task.wait(0.005)
-game:GetService("VirtualInputManager"):SendMouseButtonEvent(1000, 1000, Enum.UserInputType.MouseButton1, false, 0)
+game:GetService("VirtualInputManager"):SendMouseButtonEvent(1000, 1000, 1, false, game, 1)
 local args = {"Hit", workspace:FindFirstChild("TrackGloveMissile")}
 game:GetService("Players").LocalPlayer.Character:WaitForChild("Lantern"):WaitForChild("Network"):FireServer(unpack(args))
               task.wait(0.01)
