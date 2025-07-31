a03d50738fd86f2251a2bcf89564bef82d0d9e27
water.5202 <yeahidkorb@gmail.com>
2025-07-31 11:57:07 +0100

    KSUI.lua

--- KSUI.lua
+++ KSUI.lua
@@ -2,4 +2,4 @@ if not game:IsLoaded() then
 game.Loaded:Wait()
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/Slap_Battles.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/Slap_Battles.lua"))() -- UI IS NOT MINE CREDITS TO GIANG FOR ALL SCRIPTS RELATED TO THIS UI
