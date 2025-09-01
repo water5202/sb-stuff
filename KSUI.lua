a6ce037ae240f07a3537288ad3ea15f65f5f28da
water.5202 <yeahidkorb@gmail.com>
2025-09-01 16:29:42 +0100

    Delete KSUI.lua

--- KSUI.lua
+++
@@ -1,5 +0,0 @@
-if not game:IsLoaded() then
-game.Loaded:Wait()
-end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/Slap_Battles.lua"))() -- UI IS NOT MINE CREDITS TO GIANG FOR ALL SCRIPTS RELATED TO THIS UI
